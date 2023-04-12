70a2cf94580615e58bcea05442dba178170557ef
cherryReptile <lisovskiy.nikitk@gmail.com>
2023-04-12 03:26:53 +0300

    responses can be written to files(markdown and html)

--- cli/cmd/chat.go
+++ cli/cmd/chat.go
@@ -3,22 +3,27 @@ package cmd
 import (
 	"bufio"
 	"fmt"
-	"github.com/MichaelMure/go-term-markdown"
+	term_markdown "github.com/MichaelMure/go-term-markdown"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 	"github.com/pavel-one/EdgeGPT-Go"
 	"github.com/pavel-one/EdgeGPT-Go/internal/Logger"
 	"github.com/spf13/cobra"
-	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 var (
-	chat *EdgeGPT.GPT
-	r    bool
+	chat   *EdgeGPT.GPT
+	r      bool
+	toHtml bool
+	output string
 )
 
 var ChatCmd = &cobra.Command{
-	Use:   "Chat",
+	Use:   "chat",
 	Short: "Edge Bing chat",
 	Long:  "Simple cli for speaking with EdgeGPT Bing ",
 	Run:   runChat,
@@ -27,17 +32,14 @@ var ChatCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(ChatCmd)
 	ChatCmd.Flags().BoolP("rich", "r", false, "parse markdown to terminal")
+	ChatCmd.Flags().BoolP("html", "", false, "parse markdown to html(use with --output)")
+	ChatCmd.Flags().StringP("output", "o", "terminal", "output file(markdown or html like test.md or test.html) or terminal")
 	logger = Logger.NewLogger("Chat")
 	storage = EdgeGPT.NewStorage()
 }
 
 func runChat(cmd *cobra.Command, args []string) {
-	rich, err := cmd.Flags().GetBool("rich")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	r = rich
-
+	getFlags(cmd)
 	newChat("chat")
 
 	reader := bufio.NewReader(os.Stdin)
@@ -56,6 +58,26 @@ func runChat(cmd *cobra.Command, args []string) {
 	}
 }
 
+func getFlags(cmd *cobra.Command) {
+	rich, err := cmd.Flags().GetBool("rich")
+	if err != nil {
+		logger.Fatalf("failed to get flag `rich`: %v", err)
+	}
+	r = rich
+
+	tohtml, err := cmd.Flags().GetBool("html")
+	if err != nil {
+		logger.Fatalf("failed to get flag `html`: %v", err)
+	}
+	toHtml = tohtml
+
+	out, err := cmd.Flags().GetString("output")
+	if err != nil {
+		logger.Fatalf("failed to get flag `output`: %v", err)
+	}
+	output = out
+}
+
 func newChat(key string) {
 	gpt, err := storage.GetOrSet(key)
 	if err != nil {
@@ -65,12 +87,14 @@ func newChat(key string) {
 }
 
 func ask(input string) {
-	if r {
-		rich(input)
-		return
+	if output == "terminal" {
+		if r {
+			rich(input)
+		} else {
+			base(input)
+		}
 	} else {
-		base(input)
-		return
+		askAndWrite(input)
 	}
 }
 
@@ -102,22 +126,15 @@ func base(input string) {
 		l = anslen
 		fmt.Print(res)
 	}
+	return
 }
 
 func rich(input string) {
 	fmt.Println("Bot:")
 
-	mw, err := chat.AskSync(input)
-	if err != nil {
-		logger.Fatalln(err)
-	}
+	ans := answer(input)
 
-	ans := mw.Answer.GetAnswer()
-	if ans == "" {
-		return
-	}
-
-	result := markdown.Render(ans, 150, 4)
+	result := term_markdown.Render(ans, 150, 4)
 
 	if result == nil {
 		fmt.Println(ans)
@@ -127,4 +144,73 @@ func rich(input string) {
 	fmt.Print(string(result))
 
 	return
+}
+
+func answer(input string) string {
+	mw, err := chat.AskSync(input)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+
+	ans := mw.Answer.GetAnswer()
+	if ans == "" {
+		logger.Fatalf("failed to get answer")
+	}
+
+	return ans
+}
+
+func askAndWrite(input string) {
+	ans := []byte(answer(input))
+	if toHtml {
+		data := mdToHtml(ans)
+		writeToFile(data)
+	} else {
+		writeToFile(ans)
+	}
+}
+
+func mdToHtml(md []byte) []byte {
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
+
+func writeToFile(data []byte) {
+	_, err := os.Stat(output)
+	if os.IsNotExist(err) {
+		dir := filepath.Dir(output)
+		if dir != "." {
+			if err = os.MkdirAll(dir, 0755); err != nil {
+				logger.Fatalf("failed to create dir %s: %v", dir, err)
+			}
+		}
+
+		if err := os.WriteFile(output, data, 0644); err != nil {
+			logger.Fatalf("failed while write data to file `%s`: %v", output, err)
+		}
+	} else {
+		file, err := os.OpenFile(output, os.O_WRONLY|os.O_APPEND, 0755)
+		defer file.Close()
+
+		if err != nil {
+			logger.Fatalf("failed to open file `%s`: %v", output, err)
+		}
+
+		_, err = file.Write(data)
+		if err != nil {
+			logger.Fatalf("failed to write string to file `%s`: %v", file.Name(), err)
+		}
+
+		if err = file.Sync(); err != nil {
+			logger.Fatalf("failed to sync data for file `%s`: %v", file.Name(), err)
+		}
+	}
+	fmt.Println("    Response written successfully to " + output)
 }
